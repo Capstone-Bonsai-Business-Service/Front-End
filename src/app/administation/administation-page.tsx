97799e4628de7f89695e157e2377878f4ebaed26
Casber <61676624+hai15021999@users.noreply.github.com>
2023-06-16 21:22:08 +0700

    update form new account

--- src/app/administation/administation-page.tsx
+++ src/app/administation/administation-page.tsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { IUser } from '../../IApp.interface';
-import { Avatar, Badge, Button, Col, Divider, Dropdown, Layout, Menu, MenuProps, Row, Skeleton, Table, Tag, Tooltip } from 'antd';
-import { BarsOutlined, BellOutlined, CalendarOutlined, DeleteOutlined, DownOutlined, FormOutlined, LogoutOutlined, TableOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Col, Divider, Dropdown, Layout, Menu, MenuProps, Row, Skeleton, Table, Tag, Tooltip } from 'antd';
+import { BarsOutlined, CalendarOutlined, DeleteOutlined, FormOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import { adminServices } from './administation.service';
 import { layoutMode } from '../common/interfaces';
 import { ColumnsType } from 'antd/es/table';
 import Search from 'antd/es/input/Search';
+import { ColumnFilterItem } from 'antd/es/table/interface';
+import { uniqBy } from 'lodash';
+import { DateTime } from 'luxon';
+import { CreateAccountDialog } from './dialogs/add-account-dialog';
 
 interface IAdminPageProps {
     currentUser?: IUser;
+}
+
+interface ITableFilter {
+    fullName: ColumnFilterItem[],
+    email: ColumnFilterItem[],
+    status: ColumnFilterItem[],
+    phone: ColumnFilterItem[]
 }
 
 export const AdminPage: React.FC<IAdminPageProps> = (props) => {
@@ -20,18 +31,28 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
     const [isFirstInit, setFirstInit] = useState<boolean>(false);
     const [isDataReady, setDataReady] = useState<boolean>(false);
     const [mode, setMode] = useState<layoutMode>('list');
+    const [accountsOnSearch, setAccountOnSearch] = useState<any[]>([]);
+    const [objectFilter, setObjectFilter] = useState<ITableFilter>({
+        email: [],
+        fullName: [],
+        phone: [],
+        status: []
+    })
+    const [isShowDialogCreate, setShowDialogCreate] = useState<boolean>(false);
 
     useEffect(() => {
         if (!isFirstInit) {
             adminService.getAccounts$().subscribe({
                 next: (res) => {
                     setAccount(res);
+                    setAccountOnSearch(res);
+                    bindingObjectFilter(res);
                     setFirstInit(true);
                     setDataReady(true);
                 }
             })
         }
-    }, [adminService, isFirstInit]);
+    }, [adminService, bindingObjectFilter, isFirstInit]);
 
     const items: MenuProps['items'] = [
         {
@@ -45,6 +66,16 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
             ),
             type: 'group',
             children: [
+                {
+                    key: 'allAccounts',
+                    label: 'Tất cả',
+                    className: '__app-children-menu-divider'
+                },
+                {
+                    key: 'ownerAccounts',
+                    label: 'Tài khoản Owner',
+                    className: '__app-children-menu-divider'
+                },
                 {
                     key: 'managerAccounts',
                     label: 'Tài khoản Manager',
@@ -93,20 +124,127 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
         {
             title: 'Họ & Tên',
             dataIndex: 'fullName',
+            key: 'fullName',
+            showSorterTooltip: false,
+            sorter: {
+                compare: (acc, cur) => acc.fullName > cur.fullName ? 1 : acc.fullName < cur.fullName ? -1 : 0
+            },
+            ellipsis: true,
+            render: (value, record, index) => {
+                return <div>
+                    <Avatar src={record.avatar} />
+                    <span className='__app-column-full-name'>{value}</span>
+                </div>
+            },
+            filterMode: 'menu',
+            filters: objectFilter.fullName,
+            filterSearch: true,
+            filterMultiple: true,
+            onFilter: (value, record) => (record.fullName as string).toLowerCase().indexOf((value as string).toLowerCase()) > -1,
+
         },
         {
             title: 'Email',
             dataIndex: 'email',
+            key: 'email',
+            showSorterTooltip: false,
+            sorter: {
+                compare: (acc, cur) => acc.email > cur.email ? 1 : acc.email < cur.email ? -1 : 0
+            },
+            ellipsis: true,
+            width: '30%',
+            filterMode: 'menu',
+            filters: objectFilter.email,
+            filterSearch: true,
+            filterMultiple: true,
+            onFilter: (value, record) => (record.email as string).toLowerCase().indexOf((value as string).toLowerCase()) > -1,
         },
         {
             title: 'Số điện thoại',
             dataIndex: 'phone',
+            key: 'phone',
+            showSorterTooltip: false,
+            sorter: {
+                compare: (acc, cur) => acc.phone > cur.phone ? 1 : acc.phone < cur.phone ? -1 : 0
+            },
+            ellipsis: true,
+            width: 200,
+            filterMode: 'menu',
+            filters: objectFilter.phone,
+            filterSearch: true,
+            filterMultiple: true,
+            onFilter: (value, record) => (record.phone as string).toLowerCase().indexOf((value as string).toLowerCase()) > -1,
         },
         {
             title: 'Trạng thái',
             dataIndex: 'status',
+            key: 'status',
+            showSorterTooltip: false,
+            sorter: {
+                compare: (acc, cur) => acc.status > cur.status ? 1 : acc.status < cur.status ? -1 : 0
+            },
+            ellipsis: true,
+            render: (value) => {
+                return <Tag color={statusColorMapping(value)}>{value}</Tag>
+            },
+            width: 200,
+            filterMode: 'menu',
+            filters: objectFilter.status,
+            filterSearch: true,
+            filterMultiple: true,
+            onFilter: (value, record) => (record.status as string).toLowerCase().indexOf((value as string).toLowerCase()) > -1,
+        },
+        {
+            title: '',
+            dataIndex: 'command',
+            align: 'center',
+            width: 100,
+            key: 'command',
+            showSorterTooltip: false,
+            ellipsis: true,
+            render: (_, record, __) => {
+                return <div>
+                    <Button className='__app-command-button' onClick={(e) => {
+                        e.preventDefault();
+                        openDetailUser(record.Id);
+                    }} icon={<FormOutlined />} />
+                </div>
+            },
         }
     ]
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    function bindingObjectFilter(arrAccounts: any[]) {
+        const __objFilter = arrAccounts.reduce((acc, cur) => {
+            acc.email.push({
+                text: cur.email,
+                value: cur.email
+            });
+            acc.fullName.push({
+                text: cur.fullName,
+                value: cur.fullName
+            });
+            acc.phone.push({
+                text: cur.phone,
+                value: cur.phone
+            });
+            acc.status.push({
+                text: cur.status,
+                value: cur.status
+            });
+            return acc;
+        }, {
+            email: [],
+            fullName: [],
+            phone: [],
+            status: []
+        });
+        __objFilter.email = uniqBy(__objFilter.email, (item: any) => item.value);
+        __objFilter.fullName = uniqBy(__objFilter.fullName, (item: any) => item.value);
+        __objFilter.phone = uniqBy(__objFilter.phone, (item: any) => item.value);
+        __objFilter.status = uniqBy(__objFilter.status, (item: any) => item.value);
+        setObjectFilter(__objFilter);
+    }
 
     function statusColorMapping(status: string) {
         switch (status) {
@@ -117,23 +255,29 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
     }
 
     function renderUserInfoFrame() {
-        const arrAccounts = accounts.reduce((acc, cur) => {
+        const arrAccounts = accountsOnSearch.reduce((acc, cur) => {
             acc.push(
-                <div className='__app-user-frame-container'>
+                <div className='__app-user-frame-container' key={cur.Id}>
                     <div className='__app-avatar-block'>
                         <Avatar size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }} src={cur.avatar} />
                     </div>
                     <div className='__app-account-info-block-container'>
                         <div className='__app-account-info-top-block'>
                             <span className='__app-full-name'>{cur.fullName}</span>
-                            <Row>
-                                <Col className='__app-account-field' span={3}>Chi nhánh:</Col>
-                                <Col span={21}>{cur.store?.name}</Col>
-                            </Row>
-                            <Row>
-                                <Col className='__app-account-field' span={3}>Địa chỉ:</Col>
-                                <Col span={21}>{cur.store?.address}</Col>
-                            </Row>
+                            {
+                                cur.store ?
+                                    <>
+                                        <Row>
+                                            <Col className='__app-account-field' span={3}>Chi nhánh:</Col>
+                                            <Col span={21}>{cur.store?.name}</Col>
+                                        </Row>
+                                        <Row>
+                                            <Col className='__app-account-field' span={3}>Địa chỉ:</Col>
+                                            <Col span={21}>{cur.store?.address}</Col>
+                                        </Row>
+                                    </>
+                                    : <></>
+                            }
                         </div>
                         <Divider className='__app-divider-no-margin'></Divider>
                         <div className='__app-account-info-bottom-block'>
@@ -149,7 +293,7 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
                                 </Row>
                                 <Row>
                                     <Col className='__app-account-field' span={6}>Ngày tạo:</Col>
-                                    <Col span={18}>{cur.createdAt}</Col>
+                                    <Col span={18}>{DateTime.fromISO(cur.createdAt).toFormat('dd-MM-yyyy HH:mm')}</Col>
                                 </Row>
                                 <Row>
                                     <Col className='__app-account-field' span={6}>Trạng thái:</Col>
@@ -193,9 +337,9 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
     }
 
     function renderShimmerLoading() {
-        const arrShimmer = ['', '', ''].reduce((acc, _) => {
+        const arrShimmer = ['', '', ''].reduce((acc, _, index) => {
             acc.push(
-                <div className='__app-user-frame-container'>
+                <div className='__app-user-frame-container' key={index}>
                     <div className='__app-avatar-block'>
                         <Skeleton.Avatar active size={80}></Skeleton.Avatar>
                     </div>
@@ -253,43 +397,102 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
         return arrShimmer;
     }
 
+    function onChangeMenuSelect(key: any) {
+        let filter = '';
+        switch (key) {
+            case 'allAccounts':
+                filter = ``;
+                break;
+            case 'ownerAccounts':
+                filter = `role='Owner'`;
+                break;
+            case 'managerAccounts':
+                filter = `role='Manager'`;
+                break;
+            case 'staffAccounts':
+                filter = `role='Staff'`;
+                break;
+            case 'customerAccounts':
+                filter = `role='Customer'`;
+                break;
+        }
+        setDataReady(false);
+        adminService.getAccounts$(filter).subscribe({
+            next: res => {
+                setAccount(res);
+                setAccountOnSearch(res);
+                bindingObjectFilter(res);
+                setDataReady(true);
+            }
+        });
+    }
+
+    function openDetailUser(userId: string) {
+
+    }
+
+    function openCreateAccount() {
+        setShowDialogCreate(true);
+    }
+
+    function onCreateAccount(data: any) {
+        setShowDialogCreate(false);
+    }
+
+    function closeDialogCreateAccount() {
+        setShowDialogCreate(false);
+    }
+
     return (
         <>
             <Layout className='__admin-layout ant-layout-has-sider'>
                 <Layout.Sider className='__app-layout-slider' trigger={null}>
-                    <div className='__app-user-info-slider'>
-                        {
-                            props.currentUser?.avatar ?
-                                <Avatar className='__app-user-avatar' src={props.currentUser?.avatar} /> :
-                                <Avatar className='__app-user-avatar'>PH</Avatar>
-                        }
-                        <Dropdown
-                            trigger={['click']}
-                            menu={{ items: userMenuItems }}
-                            placement='bottomRight'>
-                            <div className='__app-user-info-group'>
-                                <div className='__app-user-display-name-container'>
-                                    <span className='__app-user-display-name'>{props.currentUser?.fullName ?? ''}</span>
-                                    <DownOutlined />
-                                </div>
-                                <div className='__app-user-role'>{props.currentUser?.role ?? ''}</div>
-                            </div>
-                        </Dropdown>
-                    </div>
-                    <Divider className='__app-user-info-divider'></Divider>
-                    <Menu className='__app-slider-menu' mode='inline' items={items} ></Menu>
+                    <Menu className='__app-slider-menu' mode='inline' items={items} defaultSelectedKeys={['allAccounts']} onSelect={(args) => {
+                        onChangeMenuSelect(args.key);
+                    }}></Menu>
                 </Layout.Sider>
                 <Layout>
                     <Layout.Header className='__app-layout-header'>
-                        <Badge count={0}>
+
+                        {/* <Badge count={0}>
                             <Avatar shape="square" size="large" icon={<BellOutlined />} />
-                        </Badge>
+                        </Badge> */}
+
+                        <div className='__app-user-info'>
+                            {/* <span className='__app-user-display-name'>{props.currentUser?.fullName ?? ''}</span> */}
+                            <Dropdown
+                                trigger={['click']}
+                                menu={{ items: userMenuItems }}
+                                placement='bottomRight'>
+                                {
+                                    props.currentUser?.avatar ?
+                                        <Avatar className='__app-user-avatar' src={props.currentUser?.avatar} size={'large'} /> :
+                                        <Avatar className='__app-user-avatar' size={'large'}>PH</Avatar>
+                                }
+                            </Dropdown>
+                        </div>
                     </Layout.Header>
                     <Layout.Content className='__app-layout-content'>
                         <div className='__app-toolbar-container'>
                             <div className='__app-toolbar-left-buttons'>
-                                <Button shape='default' type='primary'>Tạo tài khoản</Button>
-                                <Search style={{ marginLeft: 10 }} placeholder="Tìm kiếm" />
+                                <Button shape='default' type='primary' onClick={openCreateAccount}>Tạo tài khoản</Button>
+                                <Search
+                                    style={{ marginLeft: 10 }}
+                                    placeholder="Tìm kiếm"
+                                    onSearch={(value) => {
+                                        const accountSearched = accounts.reduce((acc, cur) => {
+                                            if (cur.fullName.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                                                acc.push(cur);
+                                            } else if (cur.phone.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                                                acc.push(cur);
+                                            } else if (cur.email.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                                                acc.push(cur);
+                                            }
+                                            return acc;
+                                        }, []);
+                                        setAccountOnSearch(accountSearched);
+                                    }}
+                                />
                             </div>
                             <div className='__app-toolbar-right-buttons'>
                                 <Tooltip placement='bottom' title='Dữ liệu bảng' arrow={true}>
@@ -332,10 +535,20 @@ export const AdminPage: React.FC<IAdminPageProps> = (props) => {
                                         tableLayout='auto'
                                         columns={tableUserColumns}
                                         className='__app-user-info-table'
-                                        dataSource={accounts}
+                                        dataSource={accountsOnSearch}
                                     ></Table>)
                             }
                         </div>
+                        {
+                            isShowDialogCreate ?
+                                <CreateAccountDialog
+                                    onCancel={closeDialogCreateAccount}
+                                    onSave={onCreateAccount}
+                                    key='createAccountDialog'
+                                /> :
+                                <></>
+                        }
+
                     </Layout.Content>
                 </Layout>
             </Layout>
--- src/app/administation/administation.service.ts
+++ src/app/administation/administation.service.ts
@@ -3,6 +3,7 @@ import { GlobalSettings } from "../global-settings";
 import { Observable } from "rxjs";
 import userInfo from '../../mock/userInfo.json';
 import accounts from '../../mock/accounts.json';
+import { CommonUtility } from "../utils/utilities";
 
 
 export class adminServices {
@@ -29,14 +30,40 @@ export class adminServices {
         })
     }
 
-    getAccounts$(options?: any) {
+    getAccounts$(options: string = '') {
         return new Observable<any[]>(obs => {
-            let url = this.globalSettings.domain + '/api/v1/accounts';
+            let url = this.globalSettings.domain + `/api/v1/accounts?${options}`
             axios.get(url).then((res) => {
                 obs.next(res.data);
                 obs.complete();
             }).catch(() => {
-                obs.next(accounts);
+                let data: any[] = [];
+                if (CommonUtility.isNullOrEmpty(options)) {
+                    data = accounts;
+                } else {
+                    const __role = options.slice(6, options.length - 1);
+                    data = accounts.reduce((acc, cur) => {
+                        if (cur.role === __role) {
+                            acc.push(cur);
+                        }
+                        return acc;
+                    }, [] as any);
+                }
+                obs.next(data);
+                obs.complete();
+            })
+        })
+    }
+
+    getAccount$(accountId: string) {
+        return new Observable<any>(obs => {
+            let url = this.globalSettings.domain + `/api/v1/accounts/${accountId}`
+            axios.get(url).then((res) => {
+                obs.next(res.data);
+                obs.complete();
+            }).catch(() => {
+                let data = accounts.find(item => item.Id === accountId);
+                obs.next(data);
                 obs.complete();
             })
         })
